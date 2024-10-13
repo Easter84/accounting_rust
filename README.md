e17c801400c0711d7a44bf9e9fb521ccdd80b823
Easter84 <timothyeaster72@gmail.com>
2024-10-13 09:57:58 -0500

    Added 3 submenus, no functionality yet.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 This is a console application for handling accounts written in Rust. It will connect to a SQLite db and contain the ability to add debtors, and income sources, and hold the ledgers. As the application grows I will add changes to this Readme.
+
+ChangeLog
+10/13/2024
+* Added a while loop so the application does not close upon selection of options.
+* Added 3 new menu options (Bills, Income, Ledger).
+* Added a constant to be used for seperating headers from the sub menus.
+* Fixed a infinte loop issue.
+* Fixed a logic problem with the sub menus not loading properly.
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::io;
 
+const DELIMETER: &str = "====================";
+
+
 fn main() {
-    options_display();
-    println!("Enter Selection:");
-    let input = get_input().trim().parse::<i32>().unwrap_or(-1);
-    match_options(input);
+    let mut app: bool = true;
+    while app {
+        options_display();
+        println!("Enter Selection:");
+        let input = get_input().trim().parse::<i32>().unwrap_or(-1);
+        app = main_menu_options(input);
+    }
+    
 }
 
 // This is used to display the options to the user
 fn options_display() {
-    let delimeter = "=".repeat(20);
-    println!("\n{}", delimeter);
+    println!("\n{}", DELIMETER);
     println!("Easters Accounting");
-    println!("{}\n", delimeter);
+    println!("{}\n", DELIMETER);
     println!("1: Bills");
     println!("2: Income");
     println!("3: Ledger");
@@ -20,16 +26,31 @@ fn options_display() {
 }
 
 // This takes a integer input from the user and redirects to the appropriate view
-fn match_options(option: i32) {
+fn main_menu_options(option: i32) -> bool {
     match  option {
-        1 => println!("You selected Bills."),
-        2 => println!("You selected Income."),
-        3 => println!("You selected Ledger."),
+        1 => {
+            println!("You selected Bills.");
+            bill_main_display();
+            true
+        },
+        2 => {
+            println!("You selected Income.");
+            income_display();
+            true
+        },
+        3 => {
+            println!("You selected Ledger.");
+            ledger_display();
+            true
+        },
         4 => {
             println!("Exiting...");
-            std::process::exit(0);
-        }
-        _ => println!("Invalid selection try again."),
+            false
+        },
+        _ => {
+            println!("Invalid selection try again.");
+            true
+        },
     }
 }
 
@@ -41,4 +62,86 @@ fn get_input() -> String {
         .expect("Failed to read");
 
     return user_input;
+}
+
+// This is used to display the menu for adding, editing or viewing bills.
+fn bill_main_display() {
+    println!("\n{}", DELIMETER);
+    println!("Billing Information");
+    println!("\n{}", DELIMETER);
+    println!("\tMenu\n");
+    println!("1: Add New Bill");
+    println!("2: View Current Bills");
+    println!("3: Edit Bill");
+    println!("4: Return");
+
+    let bill_input = get_input().trim().parse::<i32>().unwrap_or(-1);
+    match bill_input {
+        1 => {
+            println!("Adding new bill...");
+        },
+        2 => {
+            println!("Viewing Current Bills");
+        },
+        3 => {
+            println!("Editing Bill");
+        },
+        4 => {
+            println!("Returing to main menu.");
+            return;
+        },
+        _ => println!("Invalid Selection"),
+    }
+}
+
+// This is used to display the menu for adding, editing or viewing income sources.
+fn income_display() {
+    println!("\n{}", DELIMETER);
+    println!("Income Information");
+    println!("\n{}", DELIMETER);
+    println!("\tMenu\n");
+    println!("1: Add New Income");
+    println!("2: View Current Incomes");
+    println!("3: Edit Income");
+    println!("4: Return");
+
+    let income_input = get_input().trim().parse::<i32>().unwrap_or(-1);
+    match income_input {
+        1 => {
+            println!("Adding new income...");
+        },
+        2 => {
+            println!("Viewing Current incomes");
+        },
+        3 => {
+            println!("Editing income");
+        },
+        4 => {
+            println!("Returing to main menu.");
+            return;
+        },
+        _ => println!("Invalid Selection"),
+    }
+}
+
+// This is the menu for viewing ledger information.
+fn ledger_display() {
+    println!("\n{}", DELIMETER);
+    println!("Ledger Information");
+    println!("\n{}", DELIMETER);
+    println!("\tMenu\n");
+    println!("1: View Ledger");
+    println!("2: Return");
+
+    let ledger_input = get_input().trim().parse::<i32>().unwrap_or(-1);
+    match ledger_input {
+        1 => {
+            println!("View Ledger...");
+        },
+        2 => {
+            println!("Returing to main menu.");
+            return;
+        },
+        _ => println!("Invalid Selection"),
+    }
 }
